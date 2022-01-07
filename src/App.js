ce7285fb8719b2af2f98fa2dad56a87ab01c56c8
Ben Stoica <bstoica@uwm.com>
2022-01-07 14:58:39 -0500

    mapping data and clearing birthdays

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import data from "./data";
 import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className='container'>
-        <h3>0 Birthdays Today</h3>
-        <List />
-        <button onClick={() => console.log("click")}>Clear All</button>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
   );
